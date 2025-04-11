f6185a375f8d9200e1565b138870889a0f55390d
utnso <mativalto@gmail.com>
2025-04-11 14:34:35 +0000

    Error: logger no crea archivo .log

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,11 +1,11 @@
-#include <utils/hello.h>
 #include <utils/sockets.h>
-
+#include <utils/libs/logger.h>
 int main(int argc, char* argv[]) {
-    saludar("kernel");
-    iniciarServidor(puerto, log, io);//Maqueta
-    iniciarServidor(puerto, log, cpu);//Maqueta
-    crearConexion(ip, puerto, log);//Maqueta
+    t_log* logger = iniciar_logger("KERNEL", LOG_LEVEL_INFO);
+    log_info(logger, "Hola!");
+    iniciarServidor("4000", logger, "io"); //Maqueta
+    iniciarServidor("4001", logger, "cpu"); //Maqueta
+    //crearConexion(ip, puerto, logger); //Maqueta
 
     return 0;
 }
