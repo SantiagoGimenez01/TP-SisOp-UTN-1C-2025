1e1522d3333042f781b86e155048e6e29f041a5f
utnso <mativalto@gmail.com>
2025-07-17 02:57:57 +0000

    Fix: Elimino while(1) en main que causaba espera activa

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 config_cpu_t configCPU;
-t_log* logger;
+t_log *logger;
 int id_cpu;
 int socket_memoria = -1;
 int socket_dispatch = -1;
@@ -9,21 +9,22 @@ int socket_interrupt = -1;
 bool flag_desalojo = false;
 pthread_mutex_t mutex_flag_desalojo;
 
-
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
         printf("Falta pasar el ID de CPU como argumento.\n");
         return EXIT_FAILURE;
     }
-    id_cpu = atoi(argv[1]); 
+    id_cpu = atoi(argv[1]);
     pthread_mutex_init(&mutex_flag_desalojo, NULL);
-    char* path_config = string_new();
+    char *path_config = string_new();
     string_append_with_format(&path_config, "cpu%s.config", string_itoa(id_cpu));
     cargarConfiguracionCPU(path_config, id_cpu, &configCPU, &logger);
     establecerConexiones(id_cpu);
     escucharOperaciones();
-    while(1){
-        usleep(1);
-    }
+
+    pause();
+
     return 0;
 }
--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,34 +1,35 @@
 #include "main.h"
 
 config_kernel_t configKERNEL;
-t_log* logger;
+t_log *logger;
 uint32_t pid_global = 0;
 
-int main(int argc, char* argv[]) {
-    if (argc < 3) {
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
         printf("Ingrese los 2 parametros del primer proceso: %s <archivo_pseudocodigo> <tamanio_proceso>\n", argv[0]);
         return EXIT_FAILURE;
     }
-    char* archivo_pseudocodigo = argv[1];
+    char *archivo_pseudocodigo = argv[1];
     int tamanio_proceso = atoi(argv[2]);
 
     cargarConfiguracionKernel("kernel.config", &configKERNEL, &logger);
 
     log_debug(logger, "Archivo pseudocodigo INICIAL: %s", archivo_pseudocodigo);
     log_debug(logger, "Tamanio del proceso INICIAL: %d", tamanio_proceso);
-    
+
     establecerConexiones();
-    
-    //conectar_con_memoria(); // DE PRUEBA
 
-    inicializarEstados(); // aca inicializo las listas de todos los estados 
+    // conectar_con_memoria(); // DE PRUEBA
 
-    inicializar_proceso(archivo_pseudocodigo,tamanio_proceso);
+    inicializarEstados(); // aca inicializo las listas de todos los estados
+
+    inicializar_proceso(archivo_pseudocodigo, tamanio_proceso);
 
     iniciar_planificadores();
 
-    while (1) { // simular haciendo algo hasta que se conecte cpu
-        sleep(1);
-    }
+    pause();
+
     return 0;
 }
